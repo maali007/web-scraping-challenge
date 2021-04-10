d3776ac9439c545e1e650bb9f40751a56c8ba6ea
maali007 <maali007@umn.edu>
2021-04-10 11:27:06 -0500

    Scraping Mars News Site of news title and paragraph text.

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -4,12 +4,30 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-def init_browser():
-    # Replace the path with your actual path to the chromedriver
+
+def scrape_info():
+    # Path to chromedriver
     executable_path = {'executable_path': ChromeDriverManager().install()}
     return Browser('chrome', **executable_path, headless=False)
 
 mars_info = {}
+
+def scrape_mns():
+
+    browser = scrape_info()
+
+    # Use splinter module to go to visit Nasa news site
+    url = 'https://redplanetscience.com'
+    browser.visit(url)
+
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    mars_info["nt"] = soup.find('section', class_='image_and_description_container').find('div', class_='content_title').text
+
+    mars_info["pt"] = soup.find('section', class_='image_and_description_container').find('div', class_='article_teaser_body').text
+        
+    return mars_info
 
 
 
