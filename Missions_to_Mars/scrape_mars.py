dc4bc9b2ad608da513e8922bdd2aa39d99a5ed2a
maali007 <maali007@umn.edu>
2021-04-10 11:10:59 -0500

    Set chrome browser path.

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -4,3 +4,12 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
+def init_browser():
+    # Replace the path with your actual path to the chromedriver
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    return Browser('chrome', **executable_path, headless=False)
+
+mars_info = {}
+
+
+
