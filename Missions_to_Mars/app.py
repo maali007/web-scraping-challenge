5372e188a13dbfd27da503ad0a5b0b625b0c5995
maali007 <maali007@umn.edu>
2021-04-10 21:45:49 -0500

    Added scraping code to run in flask app instance.

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -10,5 +10,25 @@ app = Flask(__name__)
 # Set mongo connection in line
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mymars_app")
 
+@app.route("/")
+def index(): 
 
+    mars_info = mongo.db.mars_info.find_one()
+
+    return render_template("index.html", mars_info=mars_info)
+
+@app.route("/scrape")
+def scrape(): 
+
+    mars_info = mongo.db.mars_info
+    mars_info = scrape_mars.scrape_mns()
+    mars_info = scrape_mars.scrape_mfi()
+    mars_info = scrape_mars.scrape_mf()
+    mars_info = scrape_mars.scrape_mars_hemispheres()
+    mars_info.update({}, mars_info, upsert=True)
+
+    return redirect("/", code=302)
+
+if __name__ == "__main__": 
+    app.run(debug= True)
  
