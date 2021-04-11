7e0bb81ee11d2b2519e7b555c8b1c2383a0d83ed
maali007 <maali007@umn.edu>
2021-04-10 21:36:49 -0500

    Created Flask app instance and set mongo connection in line.

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -3,3 +3,12 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 import pymongo
+
+# Flask app instance
+app = Flask(__name__)
+
+# Set mongo connection in line
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mymars_app")
+
+
+ 
